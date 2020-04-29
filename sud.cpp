ceec3b71a5bf2e4e565b3204f36bd231490f715b
Deepika  Sharma <48624937+deepika567@users.noreply.github.com>
2020-04-29 16:39:01 +0530

    introduction

---
+++ sud.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include <windows.h>
+using namespace std;
+void intro()
+{
+cout<<"\n\t%%%%%%%%%       ##       ##  %%%%%%%%%%%  **********     %%%  %%%%    ##       ## ";
+  cout<<"\n\t%%              ##       ##    %%     %%  **********     %%%  %%%     ##       ##   ";
+  cout<<"\n\t%%%%%%%%%%      ##       ##    %%     %%  **********     %%% %%       ##       ##         ";
+  cout<<"\n\t        %%%     ##       ##    %%     %%  **********     %%%   %%%    ##       ##      ";
+  cout<<"\n\t%%%%%%%%%%%     ###########  %%%%%%%%%%%  **********     %%%   %%%%   ########### ";
+
+
+}
+int main()
+{
+    intro();
+    cout<<endl;
+    cout<<"Enter n to play the game and Q to quiet"<<endl;
+    char ch;
+    cin>>ch;
+    if(ch=='n'){
+        system("CLS");
+    }
+    else{
+        _Exit(10);
+    }
+    return 0;
+}
