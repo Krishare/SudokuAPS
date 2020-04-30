f2acf9a8ec148e112960617e7cae373a2ce9f1b5
Krishare <Krishna.hare83@gmail.com>
2020-04-30 17:06:37 +0530

    sudoku in middle

--- pro.cpp
+++ pro.cpp
@@ -440,6 +440,7 @@ void SetColor(int ForgC)
 
 void printGrid2(int grid[9][9]){
     for(int i = 0 ; i < 9 ; i++){
+        gotoxy(40, 5 + i);
         for(int j = 0 ; j < 9 ; j++){
             if(grid[i][j] == 0)
                 cout<<".";
@@ -491,8 +492,23 @@ void Game(int attempt){
         }
     }
 
-    cout<<"\nSolution : \n";
-    printGrid2(game);
+    if(blank == 0){
+        system("CLS");
+        printGrid2(game);
+        gotoxy(40,15);
+        delay(2);
+        cout << "\nCongratulations!!";
+
+    }else{
+        system("CLS");
+        gotoxy(40,2);
+        cout << "Solution : ";
+        delay(1);
+        printGrid2(game);
+        gotoxy(40,15);
+        delay(2);
+        cout << "Try next time.";
+    }
 }
 
 void StartGame(){
@@ -587,7 +603,7 @@ void intro(){
     delay(2);
     gotoxy(20,10);
     SetColor(145);
-    cout<<"ALGORITHM LAB PROJECT";
+    cout<<"ALGORITHM AND PROBLEM SOLVING LAB PROJECT";
     cout<<endl;
     delay(2);
     gotoxy(30,12);
